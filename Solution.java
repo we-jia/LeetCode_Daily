6d00555d82ca6cf97e4dd69ca10bfd80c8f18981
we-jia <61896502+we-jia@users.noreply.github.com>
2022-02-11 11:30:22 +0800

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int odd = 0;
+        int even = 0;
+        for (int i : position) {
+            if (i % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        return Math.min(odd, even);
+    }
+}
