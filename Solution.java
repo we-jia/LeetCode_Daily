283957fa8b2f24c2f6ef68f05409a2e44df7b4e3
we-jia <61896502+we-jia@users.noreply.github.com>
2022-02-22 09:27:33 +0800

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int sum = 0;
+        for (char c : columnTitle.toCharArray()) {
+            sum *= 26;
+            sum += c - 'A' + 1;
+        }
+        return sum;
+    }
+}
